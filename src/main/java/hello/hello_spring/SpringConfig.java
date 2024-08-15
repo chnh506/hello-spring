b6aeb2e55822c74c950256e9a5d27ab7af52327d
Heochan <heochan01@korea.ac.kr>
2024-08-15 17:02:23 +0900

    Spring JDBC Template

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hello_spring;
 
+import hello.hello_spring.repository.JdbcTemplateMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource datasource;
+
+    @Autowired
+    public SpringConfig(DataSource datasource) {
+        this.datasource = datasource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(datasource);
     }
+
 }
